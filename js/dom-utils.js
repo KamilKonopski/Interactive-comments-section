23003035f5e60ae3676fe9dc510dc56379eccb63
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-17 17:08:00 +0200

    add main comment elements

--- js/dom-utils.js
+++ js/dom-utils.js
@@ -39,12 +39,57 @@ const createLikesElement = (like) => {
     return likeContainer;
 };
 
+const createUserInfoElement = (avatar, username, date) => {
+    const userInfoElement = document.createElement('div');
+    userInfoElement.classList.add('comment__user-container');
+
+    const userAvatar = document.createElement('div');
+    userAvatar.classList.add('comment__user-avatar');
+    userAvatar.innerHTML = `<img class='user__avatar-image' src="${avatar}" alt="${username} avatar"/>`;
+    userInfoElement.appendChild(userAvatar);
+
+    const userName = document.createElement('span');
+    userName.classList.add('comment__user-name');
+    userName.innerText = `${username}`;
+    userInfoElement.appendChild(userName);
+
+    const userDate = document.createElement('span');
+    userDate.classList.add('comment__user-date');
+    userDate.innerText = `${date}`;
+    userInfoElement.appendChild(userDate);
+
+    return userInfoElement;
+};
+
+const createContentElement = (content, replyingTo='') => {
+    const userContent = document.createElement('p');
+    userContent.classList.add('comment__content');
+    const replyUser = replyingTo ? `<span class='content__reply-user'>@${replyingTo}</span>` : '';
+    userContent.innerHTML = `${replyUser} ${content}`;
+
+    return userContent;
+}
+
+
+const createReplyButtonElement = (user) => {
+    const replyButton = document.createElement('button');
+    replyButton.classList.add('comment__reply-btn');
+    replyButton.setAttribute('data-user', `${user}`)
+    replyButton.innerHTML = "<img class='reply-btn__image' src='../images/icon-reply.svg' alt='reply button icon'/> Reply";
+    // replyButton.addEventListener('click', () => replyComment(replyButton))
+
+    return replyButton;
+};
+
 const createCommentItemELement = (comment, currentUser) => {
     const commentMainElement = document.createElement('div');
     commentMainElement.classList.add('comment', 'comment__main');
     commentMainElement.setAttribute('data-user', `${comment.name}`);
 
     commentMainElement.appendChild(createLikesElement(comment.like));
+    commentMainElement.appendChild(createUserInfoElement(comment.avatar, comment.name, comment.date));
+    commentMainElement.appendChild(createContentElement(comment.content));
+    commentMainElement.appendChild(createReplyButtonElement(comment.name));
 
     return commentMainElement;
 };
